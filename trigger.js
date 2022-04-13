c3a624ce60402c0ad6b2866942bc2303ede3aa68
Sacul9000 <103578151+Sacul9000@users.noreply.github.com>
2022-04-13 14:21:21 +0200

    Create trigger.js

---
+++ trigger.js
@@ -0,0 +1,36 @@
+//showgobot, product attribute is used to determine what info the bot will provide
+
+const amountOfProducts = 3;
+
+//const botID for easy change
+
+var product = "";
+const chatbotBeginner = "TBD --> TestID";
+const chatbotAdvanced = "TBD --> TestID";
+const chatbotExpert = "TBD --> TestID";
+
+function chatbotAppear() {
+    //BOT ID is used to trigger different bots
+    gobot('show', getBotID(product));
+    return;
+}
+
+function getBotID(product) {
+    switch (this.product) {
+        case "Beginner":
+            return chatbotBeginner;
+            break;
+        case "Advanced":
+            return chatbotAdvanced;
+            break;
+        case "Expert":
+            return chatbotExpert;
+            break;
+        default:
+            return "invalidID"
+    }
+}
+
+function triggerChatbot(product){
+    
+}
